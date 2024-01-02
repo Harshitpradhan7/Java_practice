a9839f2ecddd3b571e68ebe8531d8d7613947a65
Harshit Pradhan <pradhanharshit089@gmai,.com>
2024-01-02 21:25:43 +0530

    java practice commit 1

---
+++ src/practice_Other_Questions/Ques_1.java
@@ -0,0 +1,10 @@
+package practice_Other_Questions;
+
+public class Ques_1 {
+    public static void main(String[] args) {
+//        The Two Sum problem asks us to find two numbers in an array that
+//        sum up to a given target value. We need to return the indices of these two numbers.
+
+
+    }
+}
